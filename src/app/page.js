c0b72b49562c35214e8687db16bd892dad6c60a0
Mahesh <uma@gmail.com>
2024-04-24 21:06:25 +0530

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,10 @@ function page() {
   return (
    <>
        <Navbar></Navbar>
-     <section className="hero" style={{marginTop: '2rem'}}>
+     <section className="hero" style={{marginTop: '1rem'}}>
       <div className="container">
         <div className="hero-copy">
-          <h1>Empower Your Digital Service <br />Defense. Now.</h1>
+          <h1>Empower Your Digital  <br />Defense. Now.</h1>
           <p>
             Byteforge is a solution designed for businesses aiming to maintain
             their confidential information securely encrypted at all moments.
