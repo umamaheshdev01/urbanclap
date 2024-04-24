c4b7151d73dae56b2be66d9097dc5ff9f0798f0b
Mahesh <uma@gmail.com>
2024-04-24 20:48:38 +0530

    ok

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+
+import '../css/navbar.css'
+
+const Navbar = () => {
+  const navbarLinks = [
+    { href: "/", text: "Home" },
+    { href: "/about", text: "About Us" },
+    { href: "/pricing", text: "Pricing" },
+    { href: "/blog", text: "Blog" },
+    { href: "/sample-blog", text: "Sample Blog" },
+    { href: "/customers", text: "Customers" },
+  ];
+
+  return (
+    <>
+     
+    <div className="navbar">
+      <div className="container">
+        <div className="navbar-logo">
+          <Link href="/"><i className="ph-fill ph-brain"></i></Link>
+        </div>
+        <div className="navbar-items">
+          <input type="checkbox" id="hamburger-toggle" />
+          <label htmlFor="hamburger-toggle" className="hamburger-icon">
+            <i className="ph-light ph-list"></i>
+          </label>
+          <ul className="navbar-links">
+            {navbarLinks.map((link, index) => (
+              <li className="navbar-item" key={index}>
+                <Link href={link.href}>
+                  {link.text}
+                </Link>
+              </li>
+            ))}
+            <li>
+              <div className="btn">
+                <a href="https://www.youtube.com/@codegrid">Sign Up</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,29 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react'
+import './css/index.css'
+import Navbar from './components/Navbar'
 
-export default function Home() {
+function page() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+   <>
+       <Navbar></Navbar>
+     <section className="hero" style={{marginTop: '2rem'}}>
+      <div className="container">
+        <div className="hero-copy">
+          <h1>Empower Your Digital <br />Defense. Now.</h1>
+          <p>
+            Byteforge is a solution designed for businesses aiming to maintain
+            their confidential information securely encrypted at all moments.
+          </p>
+          <button><a href="#">Get Started</a></button>
+        </div>
+        <div className="hero-img">
+          <img src="./assets/hero-img.png" alt="" />
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      </section>
+    
+   </>
+  )
 }
+
+export default page
